ddf49e171e3cdf9f02c2067ca233b7ff55ea5dfa
Stephen Beiter <stephenbeiter@gmail.com>
2021-02-14 22:29:30 -0800

    added manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,7 +8,14 @@ const FILES_TO_CACHE = [
   "./css/styles.css",
   "./js/idb.js",
   "./js/index.js",
-  "./icons/*.*"
+  "./icons/icon-512x512.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-72x72.png"
 ];
 
 self.addEventListener('install', function (e) {
